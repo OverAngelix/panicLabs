468973cc32004e143cace31864a0128b76ef80dc
OverAngelix <OverAngelix@hotmail.fr>
2022-10-04 22:50:09 +0200

    FEATURE : rangement des cartes dans un sens donné

--- Assets/Scripts/CardPositionScript.cs
+++ Assets/Scripts/CardPositionScript.cs
@@ -9,26 +9,19 @@ public class CardPositionScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        bool IsCorrectlyShuffle = true;
-        while (IsCorrectlyShuffle)
-        {
+       // while (IsCorrectlyShuffle)
+       // {
             listCards = shuffleCards(listCards);
             for (int i = 0; i < listSpawnCards.Count; i++)
             {
                 listCards[i].transform.position = listSpawnCards[i].transform.position;
             }
-        }
-        //ITERE POUR L4ORDRE DES CARTES
-
+        IsCorrectlyShuffle(listCards);
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < listSpawnCards.Count; i++)
-        {
-            
-        }
     }
 
 
@@ -47,4 +40,73 @@ public class CardPositionScript : MonoBehaviour
         return shuffleList;
     }
 
+
+    private bool IsCorrectlyShuffle(List<GameObject> listCards)
+    {
+        for (int i = 0; i < listCards.Count; i++)
+        {
+            //SI LABS
+            //SI VENTS
+            //SI CHANGES
+
+
+            if (getElement(listCards, i).tag=="RedLab" || getElement(listCards, i).tag == "BlueLab" || getElement(listCards, i).tag == "YellowLab")
+            {
+                if (getPreviousElement(listCards, i, true).tag == "RedLab" || getPreviousElement(listCards, i, true).tag == "BlueLab" || getPreviousElement(listCards, i, true).tag == "YellowLab" || getNextElement(listCards, i, true).tag == "RedLab" || getNextElement(listCards, i, true).tag == "BlueLab" || getNextElement(listCards, i, true).tag == "YellowLab")
+                {
+                    print("Coucouy");
+                }
+            }
+           /* if (getElement(listCards, i).tag == "ChangeColor" || getElement(listCards, i).tag == "ChangeForm" || getElement(listCards, i).tag == "ChangePattern")
+            {
+
+            }*/
+
+
+
+            if (getElement(listCards,i).tag == getNextElement(listCards, i,true).tag)
+            {
+                print("Blob");
+            }
+            if (getElement(listCards, i).tag == getPreviousElement(listCards, i, true).tag)
+            {
+                print("Blob2");
+            }
+
+        }
+        return true;
+    }
+
+
+    private GameObject getElement(List<GameObject> listCards,int idx)
+    {
+        return listCards[(idx + 25) % 25];
+    }
+
+    //sens true => sens horaire //sens false => sens anti-horaire
+    private GameObject getNextElement(List<GameObject> listCards, int idx, bool sens)
+    {
+        if (sens)
+        {
+            return listCards[(idx + 26) % 25];
+        }
+        else
+        {
+            return listCards[(idx + 24) % 25];
+        }
+    }
+
+    private GameObject getPreviousElement(List<GameObject> listCards, int idx, bool sens)
+    {
+        if (sens)
+        {
+            return listCards[(idx + 24) % 25];
+        }
+        else
+        {
+            return listCards[(idx + 26) % 25];
+        }
+    }
+
+    //REGEX sur les libelles pour correspondance;
 }
